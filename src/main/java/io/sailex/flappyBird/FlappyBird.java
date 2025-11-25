5af529d5b301a9333e0e77f72af30abda3980ea5
sailex <elias.hahn@mail.de>
2025-11-25 08:24:47 +0100

    Initial commit

---
+++ src/main/java/io/sailex/flappyBird/FlappyBird.java
@@ -0,0 +1,10 @@
+package io.sailex.flappyBird;
+
+import net.fabricmc.api.ModInitializer;
+
+public class FlappyBird implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/io/sailex/flappyBird/client/FlappyBirdClient.java
@@ -0,0 +1,10 @@
+package io.sailex.flappyBird.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FlappyBirdClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
